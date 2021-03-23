0cbb7d071906946da5fa9c451cafaec334855dc4
wonseokLee97 <67217686+wonseokLee97@users.noreply.github.com>
2021-03-23 14:47:53 +0900

    Update test.py

--- wonseok970/test.py
+++ wonseok970/test.py
@@ -10,17 +10,20 @@ for loc in i:
     x1, y1, r1, x2, y2, r2 = loc
 
     dis = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-
+    
+    # 접점이 무한대인 경우
     if dis == 0 and (r1 - r2) == 0:
-        result = -1
-
+        print(-1)
+        
+    # 접점이 0개인 경우
     elif dis > (r1 + r2) or max(r1, r2) - min(r1, r2) > dis:
-        result = 0
-
+        print(0)
+        
+    # 접점이 1개인 경우
     elif dis == (r1 + r2) or max(r1, r2) - min(r1, r2) == dis:
-        result = 1
+        print(1)
 
+    # 접점이 2개인 경우
     elif dis < (r1 + r2) or max(r1, r2) - min(r1, r2) < dis:
-        result = 2
+        print(2)
 
-    print(result)
