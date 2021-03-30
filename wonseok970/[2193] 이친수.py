8b0a6899ed9f509841ea0001f9bb6175dccb158e
wonseok970 <bulnabang99@gmail.com>
2021-03-30 14:24:15 +0900

    commit

---
+++ wonseok970/[2193] 이친수.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+dp = [0] * 91
+dp[1] = 1
+dp[2] = 1
+dp[3] = 2
+
+# lst = [0] * N
+# lst[0] = 1
+# lst[1] = 0
+
+for i in range(4, N + 1):
+    dp[i] = dp[i - 2] + dp[i - 1]
+
+print(dp[N])
