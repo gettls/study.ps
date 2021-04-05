f44ccee886c9342b3f49d9169a7979b1f89ff103
wonseok970 <bulnabang99@gmail.com>
2021-04-06 00:16:31 +0900

    commit

---
+++ wonseok970/Greedy/[13305] 주유소.py
@@ -0,0 +1,17 @@
+N = int(input())
+dis = list(map(int, input().split()))
+price = list(map(int, input().split()))
+total = 0
+
+for i in range(N - 1):
+    if price[i] < price[i + 1]:
+        price[i + 1] = price[i]
+
+for i in range(N - 1):
+    total += price[i] * dis[i]
+
+print(total)
+
+#   2   3   1
+# 5   2   4   1
+# 5*2 / 2*(3+1)
