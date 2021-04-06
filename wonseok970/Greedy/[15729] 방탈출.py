a848bc837b915dd52ee097975d16c524d664e4a9
wonseok970 <bulnabang99@gmail.com>
2021-04-06 15:45:47 +0900

    commit

---
+++ wonseok970/Greedy/[15729] 방탈출.py
@@ -0,0 +1,14 @@
+N = int(input())
+lst = list(map(int, input().split()))
+start = [0] * (N + 2)
+count = 0
+
+for i in range(N):
+    if lst[i] != start[i]:
+        count += 1
+        start[i] = int(not start[i])
+        start[i+1] = int(not start[i+1])
+        start[i+2] = int(not start[i+2])
+
+
+print(count)
