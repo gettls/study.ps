b644ecf0fe11fc2a43f5f179e2dd550ebadd28dc
wonseok970 <bulnabang99@gmail.com>
2021-05-27 17:19:53 +0900

    commit

--- wonseok970/문자열/[1316] 그룹 단어 체커.py
+++ wonseok970/문자열/[1316] 그룹 단어 체커.py
@@ -1,10 +1,27 @@
+from collections import deque
+
 n = int(input())
 
-for _ in range(n):
-    word = input()
-    for i in range(len(word)-1):
-        if word[i] != word[i+1]:
-            if word[i] in word[i+1:]:
-                n -= 1
-                break
-print(n)
+
+a = deque(deque(input()) for i in range(n))
+b = deque([])
+c = deque([])
+
+
+for i in range(len(a[0])):
+    get = a[0].popleft()
+    if get == b[i-1]:
+        continue
+    else:
+        b.append(get)
+print(b)
+
+for i in range(len(b)):
+    get = b.popleft()
+    print(get, c)
+    if get in c:
+        print(get, c)
+        n -= 1
+    else:
+        c.append(get)
+
---
+++ wonseok970/문자열/[4949] 균형잡힌 세상.py
@@ -0,0 +1,40 @@
+while True:
+    sentence = input()
+    if sentence == '.':
+        break
+    sentence_get = []
+    answer = True
+
+    for i in sentence:
+        # 괄호를 찾으면 기록
+        if i == "(" or i == "[":
+            sentence_get.append(i)
+        # 상반되는 기호를 찾았을 때,
+        elif i == ")":
+            # 기록이 없다면 대칭 X
+            if len(sentence_get) == 0:
+                answer = False
+                break
+            # 가장 마지막 기록이 상반되는 기호의 대칭이라면 O
+            if sentence_get[-1] == "(":
+                sentence_get.pop()
+            else:
+                answer = False
+                break
+
+        elif i == "]":
+            # 기록이 없다면 대칭 X
+            if len(sentence_get) == 0:
+                answer = False
+                break
+            # 가장 마지막 기록이 상반되는 기호의 대칭이라면 O
+            if sentence_get[-1] == "[":
+                sentence_get.pop()
+            else:
+                answer = False
+                break
+        # True and True
+        if answer and not sentence_get:
+            print("yes")
+        else:
+            print("no")
