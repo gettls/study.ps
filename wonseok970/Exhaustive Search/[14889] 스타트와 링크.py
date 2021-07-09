468e0fde72963b000a34ec5bd9427610ede7a801
wonseok <bulnabang99@gmail.com>
2021-07-09 19:29:54 +0900

    완전탐색

--- wonseok970/Exhaustive Search/[14889] 스타트와 링크.py
+++ wonseok970/Exhaustive Search/[14889] 스타트와 링크.py
@@ -1,60 +1,30 @@
-N = int(input())
 from itertools import combinations
 
+N = int(input())
+
 ability = []
-power_list = []
+player = [i for i in range(1, N+1)]
+comb = list(combinations(player, N // 2))
+min_val = 1e9
 
 for i in range(N):
     ability.append(list(map(int, input().split())))
 
-min_value = 200
 
-for i in range(0, N):
-    for j in range(i + 1, N):
-        team_power = ability[i][j] + ability[j][i]
-        power_list.append(team_power)
-# print(power_list)
-# power_list.sort()
+for team_a in comb:
+    power_a, power_b = 0, 0
+    for x in team_a:
+        for y in team_a:
+            power_a += ability[x-1][y-1]
 
-for i in range(len(power_list)-1):
-    pw_gap = abs(power_list[i] - power_list[i+1])
-    if pw_gap < min_value:
-        min_value = pw_gap
+    team_b = [i for i in range(1, N+1) if i not in team_a]
 
-print(list(combinations(power_list, N // 2)))
+    for x in team_b:
+        for y in team_b:
+            power_b += ability[x-1][y-1]
 
-for a in combinations(power_list, N//2):
-    print(sum(a))
+    min_val = min(min_val, abs(power_a - power_b))
 
+print(min_val)
 
-print(power_list)
-print(min_value)
-
-# 1 2 3
-# S12, S13, S23
-# S21, S31, S32
-
-# 1 3 6
-
-# 13, 16, 36,
-
-# S13, S16, S36
-# S31, S61, S63
-
-# 2 3 4
-# S23, S24, S34
-#
-
-
-# 3, 6, 8 = 17
-
-# 21 31 41 51 61 32 42 52 62 43 53 63 54 64 65
-#                     +
-# 12 13 14 15 16 23 24 25 26 34 35 36 45 46 56
 # [2, 3, 4, 5, 6, 4, 5, 6, 7, 6, 7, 8, 8, 9, 10]
-
-# 1 2 3 4 5 6
-# 123 124 125 126
-# 234 235 236
-# 345 346
-# 456
