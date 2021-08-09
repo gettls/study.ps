f93db01e04df04a560d1865110685c93e4e8164f
gettls <gettls@daum.net>
2021-08-09 18:09:54 +0900

    dp

---
+++ gettls/week15/[11726]2xN 타일링.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <set>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+#pragma warning (disable:4996)
+
+int dp[1001];
+int n;
+
+int main() {
+	cin >> n;
+
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+
+	cout << dp[n];
+}
