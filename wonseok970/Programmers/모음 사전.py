0328ee8dbe0b86f8372c4d0973e60dba356e0b88
wonseok <bulnabang99@gmail.com>
2021-09-07 18:56:55 +0900

    기출문제

--- wonseok970/Programmers/모음 사전.py
+++ wonseok970/Programmers/모음 사전.py
@@ -1,37 +1,27 @@
-def solution(table, languages, preference):
-    result = [[] for _ in range(len(languages))]
-    total = [0 for _ in range(len(table))]
-    index = []
-    max_value = 1e9
-    dict_language = [5, 1, 3, 4, 2]
-    table_language = ["SI", "CONTENTS", "HARDWARE", "PORTAL", "GAME"]
+# A ... AAAAE AAAAI AAAAO AAAAU
+# AAAE AAAEA AAAEE AAAEI AAAEO AAAEU
+# AAE AAEA AAEE AAEI AAEO AAEU
+# AE AEA AEE AEI AEO AEU
 
-    for i in range(len(table)):
-        table[i] = table[i].split(" ")
-        table[i].reverse()
+def solution(word):
+    dic = []
 
-    for i in range(len(languages)):
-        for j in range(len(table)):
-            if languages[i] in table[j]:
-                result[i].append((table[j].index(languages[i]) + 1) * preference[i])
-                # print(result[i])
-            else:
-                result[i].append(0)
-                # print(result[i])
+    def add(s):
+        if len(s) == 6:
+            return
+        dic.append(s)
 
-    for j in range(len(table)):
-        for i in range(len(result)):
-            total[j] += result[i][j]
+        for i in word:
+            # print('#2')
+            # print(s + i)
+            add(s + i)
 
-    max_ = max(total)
+    for i in word:
+        # print('#1')
+        # print(i)
+        add(i)
 
-    for i in range(len(total)):
-        if total[i] == max_:
-            index.append(i)
+    return dic.index(word) + 1
 
-    for i in index:
-        max_value = min(dict_language[i], max_value)
 
-    answer = table_language[dict_language.index(max_value)]
-
-    return answer
+print(solution('A'))
