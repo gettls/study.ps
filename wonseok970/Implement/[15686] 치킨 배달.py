6a72f58009ebd0e4c8a0285684c1f21de0433d5f
wonseok970 <bulnabang99@gmail.com>
2021-10-29 04:49:29 +0900

    구현

---
+++ wonseok970/Implement/[15686] 치킨 배달.py
@@ -0,0 +1,29 @@
+from itertools import combinations
+
+N, M = map(int, input().split())
+
+arr = [list(map(int, input().split())) for _ in range(N)]
+chicken = []
+house = []
+cnt = 0
+
+for i in range(N):
+    for j in range(N):
+        if arr[i][j] == 2:
+            chicken.append([i + 1, j + 1])
+        elif arr[i][j] == 1:
+            house.append([i + 1, j + 1])
+
+result = [0] * len(list(combinations(chicken, M)))
+
+for comb_c in combinations(chicken, M):
+    for h in house:
+        min_val = 2e10
+        for c in comb_c:
+            min_val = min(min_val, abs(h[0] - c[0]) + abs(h[1] - c[1]))
+        result[cnt] += min_val
+    cnt += 1
+
+print(min(result))
+
+get_some(print)
