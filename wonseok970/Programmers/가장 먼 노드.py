337da3d7fe56af3cdd81958db20fa2dc30284253
wonseok <bulnabang99@gmail.com>
2021-11-08 17:58:29 +0900

    Programmers

--- wonseok970/Programmers/가장 먼 노드.py
+++ wonseok970/Programmers/가장 먼 노드.py
@@ -3,29 +3,29 @@ from collections import deque
 
 def solution(n, edge):
     answer = 0
-    distance = [0] * (n + 1)
+    distance = [int(1e9)] * (n + 1)
     graph = [[] for _ in range(n + 1)]
 
     for v in edge:
         a, b = v
-        graph[a].append(b)
-        graph[b].append(a)
-
-    q = deque([1])
-    distance[1] = 1
-
+        graph[a].append((b, 1))
+        graph[b].append((a, 1))
+    q = deque([])
+    q.append([0, 1])
+    distance[1] = 0
+    distance[0] = 0
     while q:
-        now_node = q.popleft()
+        dist, now_node = q.popleft()
         for g in graph[now_node]:
-            next_node = g
-            if distance[next_node] == 0:
-                distance[next_node] = distance[now_node] + 1
-                q.append(g)
+            next_dist = g[1]
+            next_node = g[0]
+
+            cost = next_dist + dist
+            if distance[next_node] > cost:
+                distance[next_node] = cost
+                q.append([cost, next_node])
 
     for d in distance:
         if d == max(distance):
             answer += 1
     return answer
-
-
-print(solution(6, [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]))
